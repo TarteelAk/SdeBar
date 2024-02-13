dc28851b4a300a5afccc773f023891bfcf92f90a
TarteelAK <tarteelakram4@gmail.com>
2024-02-13 13:56:02 +0200

    first commit

---
+++ main.js
@@ -0,0 +1,4 @@
+document.getElementById("sidebar-toggle-btn").addEventListener("click", function () {
+    document.getElementById("sidebar").classList.toggle("active");
+    document.getElementById("sidebar-toggle-btn").classList.toggle("translate");
+  });
